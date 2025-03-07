7c4474851476ff825b8dbf8d0b9e3c4975aa77dc
nick134920 <7234653+nick134920@users.noreply.github.com>
2025-03-07 13:05:33 +0800

    fix: 修正脚本获取ASN ip段错误

--- ros-dpbr.sh
+++ ros-dpbr.sh
@@ -4,11 +4,10 @@ cd ./pbr
 
 wget --no-check-certificate -c -O CN.txt https://metowolf.github.io/iplist/data/special/china.txt
 
-# 获取ASN37963的IP段
-wget --no-check-certificate -c -O asn37963.json "https://stat.ripe.net/data/announced-prefixes/data.json?resource=AS37963"
+echo "" >> CN.txt
 
-# 提取IPv4前缀
-grep -o '"prefix":"[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+/[0-9]\+"' asn37963.json | sed 's/"prefix":"//g' | sed 's/"//g' > asn37963.txt
+curl -s "https://stat.ripe.net/data/announced-prefixes/data.json?resource=AS37963" | jq -r '.data.prefixes[].prefix | select(test("^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}/\\d{1,2}$"))' > asn37963.txt
+
 
 # 合并IP段
 cat CN.txt asn37963.txt | sort | uniq > merged.txt
